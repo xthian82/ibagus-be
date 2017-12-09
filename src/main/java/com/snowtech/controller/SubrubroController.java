2a59e2d50b71f770952bce47f1ff4110dc1f68f3
Cristhian Recalde <cristhian.recalde@aurea.com>
2017-12-09 15:45:47 -0300

    removed

--- src/main/java/com/snowtech/controller/SubrubroController.java
+++ src/main/java/com/snowtech/controller/SubrubroController.java
@@ -2,14 +2,13 @@ package com.snowtech.controller;
 
 import com.snowtech.entity.Subrubro;
 import com.snowtech.service.SubrubroService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +30,7 @@ public class SubrubroController {
         return subrubroService.getSubrubro(id);
     }
 
-    @PutMapping
+    @PostMapping
     public Subrubro save(@RequestBody Subrubro data) {
         return subrubroService.save(data);
     }
